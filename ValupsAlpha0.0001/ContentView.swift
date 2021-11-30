6653f689c243951669f1d929ec62808f4f44a1df
dze <zehedominik@googlemail.com>
2021-11-30 05:47:50 -0800

    Initial Commit

---
+++ ValupsAlpha0.0001/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ValupsAlpha0.0001
+//
+//  Created by Dominik on 30.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ValupsAlpha0.0001/ValupsAlpha0_0001App.swift
@@ -0,0 +1,17 @@
+//
+//  ValupsAlpha0_0001App.swift
+//  ValupsAlpha0.0001
+//
+//  Created by Dominik on 30.11.21.
+//
+
+import SwiftUI
+
+@main
+struct ValupsAlpha0_0001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
